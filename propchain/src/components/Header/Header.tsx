8ff45b924f6b52b416297d452a2a0b743e7152c5
naufal <89512179+nomsf@users.noreply.github.com>
2024-12-28 02:15:36 +0700

    add header

---
+++ propchain/src/components/Header/Header.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Box, Button, CssBaseline, Drawer, Dropdown, Input, List, ListItem, ListItemButton, Menu, MenuButton, MenuItem, Stack, Typography } from "@mui/joy";
+import Image from "next/image";
+import { useState } from "react";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#ededed",
+        },
+    }
+})
+
+
+const Header = () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Stack
+            direction="row" 
+            sx={{width: 1,
+                height: 1,
+                maxHeight: 70,
+                p: 2,
+                justifyContent: "space-between",
+                alignItems: "center",
+                backgroundColor: "#0a0a0a",
+                borderBottom: "1px solid #333",
+                }}>
+            {/* <a onClick={() => setOpen(true)}>
+                <Image
+                    src="/util/list.svg"
+                    alt="Menu"
+                    width={30}
+                    height={30}
+                />
+            </a> */}
+
+            <Dropdown>
+                <MenuButton sx={{border: "none", }}>
+                    <Image
+                        src="/util/list.svg"
+                        alt="Menu"
+                        width={30}
+                        height={30}
+                    />
+                </MenuButton>
+                <Menu>
+                    <MenuItem>Home</MenuItem>
+                    <MenuItem>User</MenuItem>
+                    <MenuItem>Ownership</MenuItem>
+                </Menu>
+            </Dropdown>
+
+            <Typography 
+                level="h1"
+                fontSize="xl"
+                fontWeight="xl"
+                color="neutral"    
+                fontFamily="Monospace"
+                textColor="#ededed"      
+            >Propchain
+            </Typography>
+            
+            <Input 
+                placeholder="Search"
+                variant="soft"
+                sx={{width: 1/3,
+                    maxWidth: 400,
+                    backgroundColor: "#333",
+                    color: "#fff",
+                }}
+                startDecorator={
+                    <img src="/util/search.svg" 
+                        alt="Search" 
+                        width={20} 
+                        height={20} 
+                        className="opacity-45"
+                        />
+                }
+            />
+        </Stack>
+    );
+}
+
+export default Header;
